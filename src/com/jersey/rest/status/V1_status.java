36b78105a5d5b6b9499c61a82bde2c24570ddfcf
rahul <rpisal@gmail.com>
2013-09-20 13:53:15 +0530

    finished ep2 of tutorial

---
+++ src/com/jersey/rest/status/V1_status.java
@@ -0,0 +1,50 @@
+package com.jersey.rest.status;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * This is the root path for our restful api service In the web.xml file, we
+ * specified that /api/* need to be in the URL to get to this class.
+ * 
+ * We are versioning the class in the URL path. This is the first version v1.
+ * Example how to get to the root of this api resource:
+ * http://localhost:7001/RESTSample/api/v1/status
+ * 
+ * @author Rahul Pisal
+ * 
+ */
+@Path("/v1/status")
+public class V1_status {
+	private static final String api_version = "00.02.00"; // version of the api
+
+	/**
+	 * This method sits at the root of the api. It will return the name of this
+	 * api.
+	 * 
+	 * @return String - Title of the api
+	 */
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String returnTitle() {
+		return "<p>Java Web Service</p>";
+	}
+
+	/**
+	 * This method will return the version number of the api Note: this is
+	 * nested one down from the root. You will need to add version into the URL
+	 * path.
+	 * 
+	 * Example: http://localhost:7001/RESTSample/api/v1/status/version
+	 * 
+	 * @return String - version number of the api
+	 */
+	@Path("/version")
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String returnVersion() {
+		return "<p>Version:</p>" + api_version;
+	}
+}
